9b3f26f523aab7af588a997fae0d0d5bc630d906
wbuz24 <willbuziak@gmail.com>
2025-02-21 12:31:11 -0700

    Deploy

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const projects = [
+  {
+    title: "Bahar Lab",
+    subtitle: "Computer Architecture Design",
+    description:
+      "Microarchitecture Simulation of RISC-V Trusted Execution Environments"
+  },
+  {
+    title: "TENNLab",
+    subtitle: "Neuromorphic Computing",
+    description:
+      "Embedded computing and robotic design for neuromorphic computing applications"
+    image: "./rob1.jpg",
+    link: "https://neuromorphic.eecs.utk.edu/",
+  },
+  {
+    title: "EESCL",
+    subtitle: "Computational Fluid Dynamics (CFD) analysis for hydrogen electrolyzers",
+    description:
+      "Simulation and modeling of multi-physics systems using both open-source tools and self-made simulations"
+    image: "./velflow.png",
+    link: "https://ecpower.utk.edu/research/",
+  },
+];
